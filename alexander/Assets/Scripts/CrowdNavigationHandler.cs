f5d4f023a097340a418d55fb588176a4f7351a94
Liam Wynn <wynnliam@pdx.edu>
2020-04-15 12:57:13 -0700

    Added members for CrowdNavigationHandler

--- alexander/Assets/Scripts/CrowdNavigationHandler.cs
+++ alexander/Assets/Scripts/CrowdNavigationHandler.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class CrowdNavigationHandler : MonoBehaviour
 {
+    private FlocksHandler flocks;
+    private NavigationMesh mesh;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        flocks = GetComponent<FlocksHandler>();
+        mesh = GetComponent<NavigationMesh>();
+
+        if (flocks != null && mesh != null)
+            Debug.Log("Got the crowd components");
+        else
+            Debug.Log("Barf!");
     }
 
     // Update is called once per frame
